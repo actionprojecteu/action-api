ae06ec5960ba7d01bf8be02e7f6d4c83e3052187
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2019-10-08 11:02:14 +0200

    password environment variable added

--- db_connection.js
+++ db_connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //Set up default mongoose connection
 var mongoDB = 'mongodb://'+process.env.MONGO_IP+'/'+process.env.MONGO_DB
-mongoose.connect(mongoDB, { useNewUrlParser: true, user: process.env.MONGO_USERNAME, pass: password });
+mongoose.connect(mongoDB, { useNewUrlParser: true, user: process.env.MONGO_USERNAME, pass: process.env.MONGO_PASSWORD });
 
 //Get the default connection
 var db = mongoose.connection;

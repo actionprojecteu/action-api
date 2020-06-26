6283465f67462c94c34aed00f952fd8d6493f610
RafaIG <rafael.ines.guillen@gmail.com>
2020-06-26 13:25:29 +0200

    Final update: PUT observation, Get images, tokens, and so on

--- application/app.py
+++ application/app.py
@@ -34,25 +34,15 @@ logging.basicConfig(filename='log/log.log', filemode='w', level=logging.INFO
 logging.info('This will get logged to a file')
 
 
-##################### Test part #####################
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 ##################### Token part #####################
-
-listOfTokens = ['Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODk5NzEyOTgsIm5iZiI6MTU4OTk3MTI5OCwianRpIjoiYTE5MzM2MTUtZmQ5NS00ODFlLWJmY2YtMjkyYTUxZDRiNGU0IiwiZXhwIjoxNTkyNTYzMjk4LCJpZGVudGl0eSI6IlBydWViYSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.ZJrBt9R0v0ZcKc9kI_6jwFCbRZECvmM6h24jafPx7m8',
-    'Bearer APiary123456']
 
 @app.before_request
 def before_request():
-    if request.method == 'POST':
+    if request.method != 'GET':
         if 'Authorization' not in request.headers:
             app.logger.warning("No auth sended.")
             return jsonify(error="No auth send."), 401
-        if request.headers['Authorization'] not in listOfTokens :
+        if mongo.db.token.count_documents({"token":request.headers['Authorization']}) == 0 :
             app.logger.warning("Token: %s not valid.", request.headers['Authorization'])
             return jsonify(error="Token not valid."), 401
 
@@ -65,8 +55,7 @@ def get_list_observation():
     page = request.args.get('page', default = 1, type=int)
 
     try:
-        begin_date = to_date(request.args.get('begin_date', default='1960-01-01'))
-            #default = date.min.strftime('%Y-%m-%d')))
+        begin_date = to_date(request.args.get('begin_date', default='0001-01-01'))
         finish_date = to_date(request.args.get('finish_date', default = date.max.strftime('%Y-%m-%d')))
     except ValueError as ex:
         return jsonify({'error': str(ex)}), 400
@@ -86,6 +75,28 @@ def get_list_observation():
     return JSONEncoder().encode(output)
 
 
+@app.route('/observations', methods=['POST'])
+def post_observation():
+    try:
+        data = request.get_json(force=True)
+    except Exception as e:
+        app.logger.warning("Failed to decode JSON object.")
+        return jsonify(error="Failed to decode JSON object."), 400 
+    if 'project' not in data:
+        return jsonify(error="Not project in the observation."), 400 
+    if 'created_at' not in data:
+        data['created_at'] = "0001-01-02T00:00:00.000000Z"
+    if 'uploaded_at' not in data:
+        data['uploaded_at'] =  datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    data.update({'created_at':datetime.strptime(data['created_at'],'%Y-%m-%dT%H:%M:%S.%fZ'),
+        'uploaded_at':datetime.strptime(data['uploaded_at'],'%Y-%m-%dT%H:%M:%S.%fZ')})
+    _id = mongo.db.observation.insert_one(data).inserted_id
+    app.logger.info('Observation %s generated successfully.', str(_id))
+    if 'image' not in data:
+        return jsonify({'id':str(_id),'warning':'no image key in the observarion'}), 201 
+    return jsonify({'id':str(_id)}), 201
+
+
 @app.route('/observations/<observation_id>', methods=['GET'])
 def get_observation(observation_id):
     try:
@@ -100,39 +111,38 @@ def get_observation(observation_id):
     return JSONEncoder().encode(observation)
 
 
-@app.route('/observations', methods=['POST'])
-def post_observation():
+@app.route('/observations/<observation_id>', methods=['PUT'])
+def put_observation(observation_id):
     try:
         data = request.get_json(force=True)
     except Exception as e:
         app.logger.warning("Failed to decode JSON object.")
-        return jsonify(error="Failed to decode JSON object."), 400 
-    if 'project' not in data:
-        return jsonify(error="Not project in the observation."), 400 
-    if 'created_at' not in data:
-        return jsonify(error="Not created_at in the observation."), 400 
-    if 'uploaded_at' not in data:
-        return jsonify(error="Not uploaded_at in the observation."), 400 
-    data.update({'created_at':datetime.strptime(data['created_at'],'%Y-%m-%dT%H:%M:%S.%fZ'),
-        'uploaded_at':datetime.strptime(data['uploaded_at'],'%Y-%m-%dT%H:%M:%S.%fZ')})
-    _id = mongo.db.observation.insert_one(data).inserted_id
-    app.logger.info('Observation %s generated successfully.', str(_id))
-    if 'image' not in data:
-        return jsonify({'id':str(_id),'warning':'no image key in the observarion'}), 201 
-    return jsonify({'id':str(_id)}), 201 
-
-
-### Observation example:
-#{
-#    "test":"yes",
-#    "project":"test",
-#    "created_at": "2019-10-12T20:57:41.479Z",
-#    "uploaded_at": "2029-10-12T22:22:46.000Z",
-#    "image": "2020-06-12-yCS14e-a.png"
-#}
+        return jsonify(error="Failed to decode JSON object."), 400
+    try:
+        success = mongo.db.observation.update({"_id": ObjectId(observation_id)}, {"$set": data})
+    except Exception as e:
+        app.logger.warning("Not a correct observation id: %s .", observation_id)
+        return jsonify(error="Not a correct observation id."), 400
+    if success.get('updatedExisting') is False:
+        app.logger.info('Observation %s not found.', observation_id)
+        return jsonify({'ok': False, 'msg': 'Observation '+ observation_id + ' not found.'})
+    app.logger.info('Observation %s updated successfully.', observation_id)
+    return jsonify({'id':observation_id, 'ok': True, 'msg': 'Observation updated successfully.'})
 
 
 ##################### image part #####################
+
+@app.route('/images', methods=['GET'])
+def get_list_images():
+    limit = request.args.get('limit', default = 5, type=int)
+    page = request.args.get('page', default = 1, type=int)
+    observations = mongo.db.image.find().skip(int(limit)*(int(page)-1)).limit(int(limit))
+    output = []
+    for ob in observations:
+        output.append(ob)
+    app.logger.info('Images send successfully.')
+    return JSONEncoder().encode(output)
+
 
 @app.route('/images', methods=['POST'])
 def post_image():
@@ -156,58 +166,11 @@ def post_image():
     _id = mongo.db.image.insert_one(data).inserted_id
     app.logger.info('image %s with id in mongodb: %s created successfully.', name, str(_id))
     return jsonify({'id':name}), 201
-        
-
-@app.route('/test', methods=['POST'])
-def process_image():
-    #f = Image.open('./img/6uYT02action-street-spectra_1.jfif')
-    #exif = { ExifTags.TAGS[k]: v for k, v in f._getexif().items() if k in ExifTags.TAGS }
-    exif = get_exif('./img/6uYT02action-street-spectra_1.jfif')
-    print(exif)
-
-    labeled = get_labeled_exif(exif)
-    print(labeled)
-
-    geotags = get_geotagging(exif)
-    print(geotags)
-
-    return jsonify(ok="ok."), 200
-
-def get_exif(filename):
-    image = Image.open(filename)
-    image.verify()
-    return image._getexif()
-
-def get_labeled_exif(exif):
-    labeled = {}
-    for (key, val) in exif.items():
-        labeled[TAGS.get(key)] = val
-    return labeled
 
 
-def get_geotagging(exif):
-    if not exif:
-        raise ValueError("No EXIF metadata found")
-
-    geotagging = {}
-    for (idx, tag) in TAGS.items():
-        if tag == 'GPSInfo':
-            if idx not in exif:
-                raise ValueError("No EXIF geotagging found")
-
-            for (key, val) in GPSTAGS.items():
-                if key in exif[idx]:
-                    geotagging[val] = exif[idx][key]
-
-    return geotagging
-
-#exif = get_exif('image.jpg')
-#geotags = get_geotagging(exif)
-#print(geotags)
-
-
-# curl -X POST -F "file=@a.png" localhost:5000/image
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'jfif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'jpe', 'gif', 'jfif', 'jif', 
+    'jfi', 'tiff', 'tiff', 'psd', 'pdf', 'eps', 'ai', 'indd', 'raw', 'svg', 
+    'heif', 'webp'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
---
+++ daemon/daemon.py
@@ -0,0 +1,101 @@
+##################### Imports #####################
+
+import logging
+from pymongo import MongoClient
+
+from PIL import Image
+from PIL.ExifTags import TAGS,GPSTAGS
+
+
+##################### logging part #####################
+
+logging.basicConfig(filename='log/daemon.log', filemode='w', level=logging.INFO
+	, format='%(asctime)s %(levelname)s %(name)s : %(message)s')
+logging.info('This will get logged to a file')
+
+
+##################### mongo part #####################
+
+#MONGO_URI = 'mongodb://192.168.123.245:27017/'
+MONGO_URI = 'mongodb://localhost:27017/'
+client = MongoClient(MONGO_URI)
+
+
+##################### image part #####################
+
+def process_image(filename,db):
+    data = db.observation.find_one({"image":filename})
+    filename = './img/'+ filename
+    exif = get_exif(filename)
+    if not exif:
+        return -2
+    labeled = get_labeled_exif(exif)
+    if not labeled:
+        return -3
+    data.update({'image_info':labeled})
+    db.observation.update_one({'_id':data['_id']},{"$set":data})
+
+    return 0, labeled
+
+def get_exif(filename):
+    image = Image.open(filename)
+    #image.verify()
+    image.load()
+    return image._getexif()
+
+def get_labeled_exif(exif):
+    labeled = {}
+    for (key, val) in exif.items():
+        labeled[TAGS.get(key)] = val
+    return labeled
+
+
+def get_geotagging(exif):
+    if not exif:
+        raise ValueError("No EXIF metadata found")
+
+    geotagging = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in exif:
+                raise ValueError("No EXIF geotagging found")
+
+            for (key, val) in GPSTAGS.items():
+                if key in exif[idx]:
+                    geotagging[val] = exif[idx][key]
+
+    return geotagging
+
+
+##################### main part #####################
+
+def main():
+    db = client.get_database('observation')
+    logging.info('Daemon starts')
+    for image in db.image.find({"status":"pending"}):
+        if db.observation.count_documents({"image":image['id']}) != 0:
+            logging.info('Processing image: %s',image['id'])
+            status,labeled = process_image(image['id'],db)
+            if status == 0:
+                image.update({"status":"done",'image_info':labeled})
+                logging.info('Image: %s processed correctly',image['id'])
+            if status == -2:
+                image.update({"status":"error","error":"No exif"})
+                logging.error('Error in image: %s with error: No exif',image['id'])
+            if status == -3:
+                image.update({"status":"error","error":"No labeled"})
+                logging.error('Error in image: %s with error: No labeled',image['id'])
+            if status == -4:
+                image.update({"status":"error","error":"No geotags"})
+                logging.error('Error in image: %s with error: No geotags',image['id'])
+            if status < 0:
+                image.update({"status":"error","error":"general error"})
+                logging.error('Error in image: %s with error: general error',image['id'])
+            db.image.update_one({'_id':image['_id']},{"$set":image})
+        
+
+    logging.info('Daemon finishes')
+
+
+if __name__ == "__main__":
+    main()
